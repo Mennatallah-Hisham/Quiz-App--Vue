67d3d9a0b295afe58033567302e5ab2d1874ba3e
unknown <mennahisham1d@gmail.com>
2023-09-07 15:09:50 +0200

    display quizes

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {ref,watch} from"vue";
+import q from "./data/quizes.json"
+import Card from "./components/Card.vue";
+const quizes =ref(q)
+const search=ref("");
+
+
+watch(search,()=>{
+  console.log("hello frim watch")
+  quizes.value=q.filter(quiz=>quiz.name.toLowerCase().includes(search.value.toLowerCase()));
+
+})
+</script>
+<template>
+<div class="container">
+
+
+  <header>
+    <h1> quiz app</h1>
+ 
+    <form>
+      <input v-model.trim="search" type="text"/>
+    </form>
+
+  </header>
+  
+  <div class="options-container">
+    <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+  
+
+   
+
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+.options-container{
+  border:1px solid black;
+  display: flex;
+  flex-wrap: wrap;
+  gap:1rem;
+
+}
+
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps(['quiz'])
+</script>
+<template>
+
+    
+    <article class="card">
+        <img :src="quiz.img" alt="cover image"/>
+  
+        <h2>{{quiz.name}}</h2>
+        <p> {{quiz.questions.length}} questions</p>
+  
+      </article>
+</template>
+
+<style scoped>
+.card{
+    width:22rem;
+    padding:1rem;
+    border:1px solid red;
+    overflow:hidden;
+    border-radius: 2rem;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  }
+  .card img{
+    max-width: 100%;
+    display: block;
+  }</style>
